030a1246242bb8e3b81dd6e7c2ecce5a091d1f7d
HarnoorK29 <157150277+HarnoorK29@users.noreply.github.com>
2024-04-06 23:17:46 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,77 +2,59 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
- * @author Harnoor Kaur Gill and Mehakdeep Kaur
- * @date 18th february 2024
+ * @author by Harnoor Kaur Gill and Mehakdeep Kaur
+ * @date 5thApril,2024
  */
 package ca.sheridancollege.project;
 
 /**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ * A class is used as the base Card class for the project..
  *
- * @author dancye
+ * 
+ * 
  */
 public  class Card {
     
-     private Value value ;
-    private Suit suit;
-    
-    public enum Suit 
-    {
-        HEARTS,CLUBS ,SPADES ,DIAMONDS
+
+    private Value value;
+    private Suit suit ;
+    public enum Suit{
+        HEARTS , CLUBS , DIAMONDS , SPADES 
     }
     public enum Value
     {
-        ACE , TWO, THREE,FOUR,FIVE, SIX,SEVEN,EIGHT,NINE , TEN , JACK , QUEEN ,KING 
+          ACE, TWO, THREE, FOUR, FIVE,SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
     }
     
-    
-     /**
-     *@param givenValue
-     **/
-    /** adding getters and setters for value and suit   * 
-     * 
-     * 
-     * @param givenValue
-     * @param givenSuit 
-     */
-    public Card(Value givenValue , Suit givenSuit)
+   public static final int[] RANKS = {1,2, 3, 4,5,6,7,8,9,10};
+   
+    public Card(Value givenValue, Suit givenSuit)
     {
-        value =givenValue ;
-        suit= givenSuit;
-        
+        value =givenValue;
+        suit=givenSuit;
     }
 
-    public Value getValue() {
-        return value;
-    }
-
-    public Suit getSuit() {
-        return suit;
-    }
-
-    public void setValue(Value value) {
-        this.value = value;
-    }
-
-    public void setSuit(Suit suit) {
-        this.suit = suit;
-    }
-    
-    
-    //default modifier for child classes
-
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
     public  String toString()
-            
-    {return suit.toString() + " - " + suit.toString();
-   }
-    
-
+    {
+        return value + "of" + suit;
+    }
+  public Value getValue()
+  {
+      return value ;
+      
+  }
+  public void setValue(Value value)
+          
+  {
+      this.value = value ;
+  }
+  public Suit getSuit()
+  {
+      return suit;
+  }
+  public void setSuit(Suit suit)
+  {
+      this.suit = suit;
+  }
 }
+
